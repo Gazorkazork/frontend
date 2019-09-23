d8b8db037910bf8d031eef195232b2fcda680bd9
rmejia-tajc <rmejia.tajc@gmail.com>
2019-09-23 16:34:04 -0600

    setup login/register

--- src/components/landing/login-modal.js
+++ src/components/landing/login-modal.js
@@ -1,21 +1,60 @@
 import React, { useState } from "react";
+import axios from "axios"
 
 const Login = () => {
 
+  const [loginState, setLoginState] = useState({username: "", password: ""})
+
+  const [registerState, setRegisterState] = useState({username: "", password1: "", password2: ""})
+
   const [isRegister, setIsRegister] = useState(false)
 
+  const handleLogin = (e) => {
+    e.preventDefault()
+    axios
+      .post('https://gazorkazork.herokuapp.com/api/login', loginState)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('token', res.key)})
+      .catch(err => console.error(err))
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+    axios
+    .post('https://gazorkazork.herokuapp.com/api/registration', registerState)
+    .then(res => {
+      console.log(res)
+      localStorage.setItem('token', res.key)})
+    .catch(err => console.error(err))
+  }
+
+  const loginChange = (e) => {
+    setLoginState({
+      ...loginState,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const registerChange = (e) => {
+    setRegisterState({
+      ...registerState,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
-    <div class="modal-wrapper">
-      <div class="login-wrapper">
-        <div class="collapsed-section hidden">
-          <div class="hide-section">
-            <h2 class="collapsed-heading">Have an Account?</h2>
-            <p class="collapsed-text">
+    <div className="modal-wrapper">
+      <div className="login-wrapper">
+        <div className="collapsed-section hidden">
+          <div className="hide-section">
+            <h2 className="collapsed-heading">Have an Account?</h2>
+            <p className="collapsed-text">
               To keep connected with us please
               <br />
               login with your personal info
             </p>
-            <button class="collapsed-btn" onclick={(e) => {
+            <button className="collapsed-btn" onClick={(e) => {
               e.preventDefault()
               setIsRegister(false)
             }}>
@@ -24,35 +63,35 @@ const Login = () => {
           </div>
         </div>
 
-        <div class="form-wrapper login">
-          <h2 class="form-heading">Log In To Your Profile</h2>
-          <form action="" class="form login">
-            <div class="input-field">
-              <i class="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" value="" />
+        <div className="form-wrapper login">
+          <h2 className="form-heading">Log In To Your Profile</h2>
+          <form className="form login" onSubmit={handleLogin}>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input placeholder="Username" onChange={loginChange} name="username" value={loginState.username} />
             </div>
 
-            <div class="input-field">
-              <i class="fas fa-key"></i>
-              <input type="password" placeholder="Password" value="" />
+            <div className="input-field">
+              <i className="fas fa-key"></i>
+              <input type="password" placeholder="Password" onChange={loginChange} name="password" value={loginState.password} />
             </div>
-            <button class="form-btn" type="submit">
+            <button className="form-btn" type="submit">
               Log In
             </button>
           </form>
         </div>
       </div>
 
-      <div class="signup-wrapper">
-        <div class="collapsed-section hidden">
-          <div class="hide-section">
-            <h2 class="collapsed-heading">New Here?</h2>
-            <p class="collapsed-text">
+      <div className="signup-wrapper">
+        <div className="collapsed-section hidden">
+          <div className="hide-section">
+            <h2 className="collapsed-heading">New Here?</h2>
+            <p className="collapsed-text">
               Create an account
               <br />
               and start playing!
             </p>
-            <button class="collapsed-btn" onclick={(e) => {
+            <button className="collapsed-btn" onClick={(e) => {
               e.preventDefault()
               setIsRegister(true)
             }}>
@@ -61,24 +100,23 @@ const Login = () => {
           </div>
         </div>
 
-        <div class="form-wrapper signup">
-          <h2 class="form-heading">Create Account</h2>
+        <div className="form-wrapper signup">
+          <h2 className="form-heading">Create Account</h2>
 
-          <form action="" class="form login">
-            <span>or use your email for registration:</span>
-            <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" placeholder="Name" value="" />
+          <form action="" className="form login" onSubmit={handleRegister}>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input placeholder="Username" onChange={registerChange} name="username" value={registerState.username} />
             </div>
-            <div class="input-field">
-              <i class="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" value="" />
+            <div className="input-field">
+              <i className="fas fa-key"></i>
+              <input type="password" placeholder="Password" onChange={registerChange} name="password1" value={registerState.password1} />
             </div>
-            <div class="input-field">
-              <i class="fas fa-key"></i>
-              <input type="password" placeholder="Password" value="" />
+            <div className="input-field">
+              <i className="fas fa-key"></i>
+              <input type="password" placeholder="Confirm Password" onChange={registerChange} name="password2" value={registerState.password2} />
             </div>
-            <button class="form-btn" type="submit">
+            <button className="form-btn" type="submit">
               Sign Up
             </button>
           </form>

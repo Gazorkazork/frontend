f1912556459dcac74e3b410d6e84d2031a25cf22
BrannanC <brannanconrad@gmail.com>
2019-09-25 17:01:10 -0500

    Parser implemented, can move and say stuff from command line

--- src/components/game/input.jsx
+++ src/components/game/input.jsx
@@ -1,25 +1,43 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import parseCommand from "../../utils/textParser";
+
 function Input(props) {
   const [userInput, setUserInput] = useState("");
-  const say = e => {
-    e.preventDefault();
-    axios
-      .post("https://gazorkazork.herokuapp.com/api/adv/say/", {
-        message: userInput
-      })
-      .then(res => setUserInput(""))
-      .catch(err => console.error(err));
-  };
 
   const handleChange = e => {
     setUserInput(e.target.value);
   };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const parsedInput = parseCommand(userInput);
+    switch (parsedInput.act) {
+      case "go":
+        axios
+          .post("https://gazorkazork.herokuapp.com/api/adv/move/", {
+            direction: parsedInput.adv
+          })
+          .then(res => {
+            console.log(res);
+            // setGameData(res.data);
+          })
+          .catch(err => console.error(err));
+      case "say":
+        axios
+          .post("https://gazorkazork.herokuapp.com/api/adv/say/", {
+            message: userInput
+          })
+          .catch(err => console.error(err));
+      default:
+    }
+    setUserInput("");
+  };
   return (
     <div className="input-container">
       <h2>This is the input box</h2>
-      <form onSubmit={say}>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="userInput"

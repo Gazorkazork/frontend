a2290efe52038ed1280c72d2110443c33cb1bd52
joshua-disney <joshua.a.disney@gmail.com>
2019-09-26 15:13:22 -0700

    Fixed data font size

--- src/components/game/data.jsx
+++ src/components/game/data.jsx
@@ -6,14 +6,14 @@ function Data({ gameData }) {
   return (
     <div className="data-container">
       <h2 className="data-heading">{gameData.name}</h2>
-      <h3>Health:</h3>
+      <h3 className="data-categories">Health:</h3>
       <p className="player">100</p>
-      <h3>Stamina:</h3>
+      <h3 className="data-categories">Stamina:</h3>
       <p className="player">250</p>
       <br />
       {gameData.players && (
         <div className="data-players">
-          <h3>Players in room:</h3>
+          <h3 className="data-categories">Players in room:</h3>
           {gameData.players.map(player => (
             <p key={player} className="player">
               {player}
@@ -21,6 +21,7 @@ function Data({ gameData }) {
           ))}
         </div>
       )}
+      <h3 className="data-categories">Items in room:</h3>
     </div>
   );
 }

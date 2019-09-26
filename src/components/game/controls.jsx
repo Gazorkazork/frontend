59a5b865b6570202a29e31cc73c03f8f4874fcf7
joshua-disney <joshua.a.disney@gmail.com>
2019-09-26 09:56:55 -0700

    Brought back logout

--- src/components/game/controls.jsx
+++ src/components/game/controls.jsx
@@ -3,20 +3,42 @@ import React from "react";
 function Controls(props) {
   return (
     <div className="controls-container">
-
       <h3 className="hub-right-heading">Controls</h3>
-
-      <div className="direction-controls">
-        <div className="direction-box" />
-        <button className="direction-box direction-button" onClick={e => props.move(e, "n")}>N</button>
-        <div className="direction-box" />
-        <button className="direction-box direction-button" onClick={e => props.move(e, "w")}>W</button>
-        <div className="direction-box" />
-        <button className="direction-box direction-button" onClick={e => props.move(e, "e")}>E</button>
-        <div className="direction-box" />
-        <button className="direction-box direction-button" onClick={e => props.move(e, "s")}>S</button>
-        <div className="direction-box" />
-        
+      <div className="controls-buttons">
+        <div className="direction-controls">
+          <div className="direction-box" />
+          <button
+            className="direction-box direction-button"
+            onClick={e => props.move(e, "n")}
+          >
+            N
+          </button>
+          <div className="direction-box" />
+          <button
+            className="direction-box direction-button"
+            onClick={e => props.move(e, "w")}
+          >
+            W
+          </button>
+          <div className="direction-box" />
+          <button
+            className="direction-box direction-button"
+            onClick={e => props.move(e, "e")}
+          >
+            E
+          </button>
+          <div className="direction-box" />
+          <button
+            className="direction-box direction-button"
+            onClick={e => props.move(e, "s")}
+          >
+            S
+          </button>
+          <div className="direction-box" />
+        </div>
+        <div className="command-buttons">
+          <button onClick={e => props.handleLogout(e)}>Logout</button>
+        </div>
       </div>
     </div>
   );
